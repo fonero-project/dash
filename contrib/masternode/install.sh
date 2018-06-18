701385e6445e408ed9ee50ce436c8e077ffe6280
Azart <39407862+azartpay@users.noreply.github.com>
2018-06-19 01:25:48 +0500

    Update install.sh

--- contrib/masternode/install.sh
+++ contrib/masternode/install.sh
@@ -12,42 +12,30 @@ fi
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt-get install nano mc htop git -y
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common -y
-sudo apt-get install libboost-all-dev -y
-sudo apt-get install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev -y
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt-get update -y
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-sudo apt-get -y install python-virtualenv
-sudo apt-get -y install ntpdate
+sudo apt-get install mc htop git python-virtualenv ntpdate -y
 sudo ntpdate -u pool.ntp.org
-cd
-basedir="/opt"
-azartdir=$basedir"/azart/"
-azartcoredir=$basedir"/.azartcore/"
-sudo mkdir $azartdir
-cd $azartdir
+sudo mkdir /opt/azart-core
+cd /opt/azart-core
 wget https://github.com/azartpay/azart/releases/download/0.12.3.3/azart-0.12.3.3-linux-x64.tgz
 tar -xvf azart-0.12.3.3-linux-x64.tgz
 rm azart-0.12.3.3-linux-x64.tgz
 mv azart-0.12.3.3-linux-x64/azartd ./azartd
 mv azart-0.12.3.3-linux-x64/azart-cli ./azart-cli
 rm -rf azart-0.12.3.3-linux-x64
-chmod -R 755 $azartdir
-cd $basedir
+chmod -R 755 /opt/azart-core
+cd /opt
 git clone https://github.com/azartpay/azart-sentinel azart-sentinel
 cd azart-sentinel
 virtualenv ./venv
 ./venv/bin/pip install -r requirements.txt
 cat <(crontab -l) <(echo "* * * * * cd /opt/azart-sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1") | crontab -
-cd $azartdir
+cd /opt/azart-core
 ./azartd -daemon
 sleep 10
 masternodekey=$(./azart-cli masternode genkey)
 ./azart-cli stop
 sleep 3
-echo -e "\ndaemon=1\nmaxconnections=256\nmasternode=1\nmasternodeprivkey=$masternodekey" >> $azartcoredir"azart.conf"
+echo -e "\ndaemon=1\nmaxconnections=256\nmasternode=1\nmasternodeprivkey=$masternodekey" >> "/root/.azartcore/azart.conf"
 sleep 3
 ./azartd -daemon
 echo "Masternode private key: $masternodekey"
