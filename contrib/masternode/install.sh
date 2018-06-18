622aba5ab3b36f29669d8db82f86a10366d6deb8
Azart <39407862+azartpay@users.noreply.github.com>
2018-06-19 00:21:05 +0500

    Update install.sh

--- contrib/masternode/install.sh
+++ contrib/masternode/install.sh
@@ -20,6 +20,8 @@ sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get -y install python-virtualenv
+sudo apt-get -y install ntpdate
+sudo ntpdate -u pool.ntp.org
 cd
 basedir="/opt"
 azartdir=$basedir"/azart/"
