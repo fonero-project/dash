0dc8add03b18d1e19e2ca9e406be2804e9598e57
Azart <39407862+azartpay@users.noreply.github.com>
2018-06-18 23:53:21 +0500

    Update install.sh

--- contrib/masternode/install.sh
+++ contrib/masternode/install.sh
@@ -12,29 +12,31 @@ fi
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt-get install nano htop git -y
+sudo apt-get install nano mc htop git -y
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common -y
 sudo apt-get install libboost-all-dev -y
 sudo apt-get install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+apt-get -y install python-virtualenv
 cd
-basedir=$(pwd)
-azartbuilddir=$basedir"/azarttemp/"
+basedir="/opt"
 azartdir=$basedir"/azart/"
 azartcoredir=$basedir"/.azartcore/"
-rm -r $azartbuilddir
-rm -r $azartdir
-rm -r $azartcoredir
 cd $azartdir
 wget https://github.com/azartpay/azart/releases/download/0.12.3.3/azart-0.12.3.3-linux-x64.tgz
 tar -xvf azart-0.12.3.3-linux-x64.tgz
 rm azart-0.12.3.3-linux-x64.tgz
-cp azart-0.12.3.3-linux-x64/azart{d,-cli} ./
+cp azart-0.12.3.3-linux-x64/azart{d,-cli} ./*
 rm -rf azart-0.12.3.3-linux-x64
 chmod -R 755 $azartdir
-mkdir $azartcoredir
+cd $basedir
+git clone https://github.com/azartpay/azart-sentinel azart-sentinel
+cd azart-sentinel
+virtualenv ./venv
+./venv/bin/pip install -r requirements.txt
+cat <(crontab -l) <(echo "* * * * * cd /opt/azart-sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1") | crontab -
 ./azartd -daemon
 sleep 10
 masternodekey=$(./azart-cli masternode genkey)
