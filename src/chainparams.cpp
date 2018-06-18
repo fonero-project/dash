4de7b9e58673e30a907d5d3f5e7de898060f79a4
Azart <39407862+azartpay@users.noreply.github.com>
2018-06-18 22:14:28 +0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -174,8 +174,9 @@ public:
             ( 0, uint256S("0x000009b9903dae4466d48db6c264d711ac554492da34cd0bfa4c0b6d230f29c9"))
             ( 5635, uint256S("0x000000000002665ec91010085bff6585a4daa4b302d4ce50eb0a297674314362")),
             ( 17724, uint256S("0x000000000001299301bc564d648abde01925a869e2100a76dcc52a75be699b29")),
+            ( 20774, uint256S("0x00000000000699d6a1f6c9403f3b77e1298a243dcbf9d1e901cc8cf3c462767c")),
             1526536800, // * UNIX timestamp of last checkpoint block
-            11423,    // * total number of transactions between genesis and last checkpoint
+            12769,    // * total number of transactions between genesis and last checkpoint
                         //   (the tx=... number in the SetBestChain debug.log lines)
             5000        // * estimated number of transactions per day after checkpoint
         };
