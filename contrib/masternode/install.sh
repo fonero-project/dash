31fc362b05d7424c5d9099b47ca877ef6ce7bd88
Fonero <35380372+fonero-project@users.noreply.github.com>
2019-01-05 06:27:05 +0500

    Update install.sh

--- contrib/masternode/install.sh
+++ contrib/masternode/install.sh
@@ -16,14 +16,14 @@ sudo apt-get install mc htop git python-virtualenv ntpdate -y
 sudo ntpdate -u pool.ntp.org
 sudo mkdir /opt/fonero-core
 cd /opt/fonero-core
-wget https://github.com/fonero-project/fonero/releases/download/v0.13.0.3/fonero-0.13.0.3-linux.tgz
-tar -xvf fonero-0.13.0.3-linux.tgz
-rm fonero-0.13.0.3-linux.tgz
-mv fonero-0.13.0.3-linux/fonerod ./fonerod
-mv fonero-0.13.0.3-linux/fonero-cli ./fonero-cli
-mv fonero-0.13.0.3-linux/fonero-tx ./fonero-tx
-mv fonero-0.13.0.3-linux/fonero-qt ./fonero-qt
-rm -rf fonero-0.13.0.3-linux
+wget https://github.com/fonero-project/fonero/releases/download/v0.13.0.4/fonero-0.13.0.4-linux.tgz
+tar -xvf fonero-0.13.0.4-linux.tgz
+rm fonero-0.13.0.4-linux.tgz
+mv fonero-0.13.0.4-linux/fonerod ./fonerod
+mv fonero-0.13.0.4-linux/fonero-cli ./fonero-cli
+mv fonero-0.13.0.4-linux/fonero-tx ./fonero-tx
+mv fonero-0.13.0.4-linux/fonero-qt ./fonero-qt
+rm -rf fonero-0.13.0.4-linux
 chmod -R 755 /opt/fonero-core
 cd /opt
 git clone https://github.com/fonero-project/fonero-sentinel fonero-sentinel
@@ -37,7 +37,7 @@ sleep 10
 masternodekey=$(./fonero-cli masternode genkey)
 ./fonero-cli stop
 sleep 3
-echo -e "server=1\nlisten=1\ndaemon=1\nmaxconnections=256\nmasternode=1\nmasternodeprivkey=$masternodekey\nrpcuser=fonerorpc\nrpcpassword=d92bb33446cefaf765ad6202f518709b\nrpcport=19192\nrpcallowip=127.0.0.1\naddnode=85.10.194.14:19190\naddnode=188.40.62.51:19190\naddnode=37.9.52.254:19190\naddnode=37.9.52.253:19190\naddnode=37.9.52.252:19190\naddnode=37.9.52.17:19190\naddnode=37.9.52.16:19190\naddnode=5.188.205.146:19190\naddnode=5.188.205.112:19190\naddnode=5.188.204.7:19190\naddnode=5.188.204.5:19190\naddnode=5.188.204.4:19190\naddnode=5.188.204.3:19190\naddnode=5.188.63.248:19190\naddnode=5.188.63.247:19190\naddnode=5.188.63.102:19190\naddnode=5.188.63.50:19190\n" >> "/root/.fonerocore/fonero.conf"
+echo -e "server=1\nlisten=1\ndaemon=1\nmaxconnections=256\nmasternode=1\nmasternodeprivkey=$masternodekey\nrpcuser=fonerorpc\nrpcpassword=d92bb33446cefaf765ad6202f518709b\nrpcport=19192\nrpcallowip=127.0.0.1\naddnode=85.10.194.14:19190\naddnode=188.40.62.51:19190\naddnode=37.9.52.254:19190\naddnode=37.9.52.253:19190\naddnode=37.9.52.252:19190\naddnode=5.188.205.146:19190\naddnode=5.188.205.112:19190\naddnode=5.188.204.7:19190\naddnode=5.188.204.5:19190\naddnode=5.188.204.4:19190\naddnode=5.188.204.3:19190\naddnode=5.188.63.248:19190\naddnode=5.188.63.247:19190\naddnode=5.188.63.102:19190\naddnode=5.188.63.50:19190\naddnode=37.9.52.17:19190\naddnode=37.9.52.16:19190\naddnode=193.47.33.49:19190\naddnode=193.47.33.48:19190\naddnode=193.47.33.47:19190\naddnode=193.47.33.46:19190\naddnode=193.47.33.45:19190\naddnode=193.47.33.44:19190\naddnode=193.47.33.43:19190\naddnode=193.47.33.42:19190\naddnode=193.47.33.41:19190\naddnode=193.47.33.40:19190\naddnode=193.47.33.39:19190\naddnode=193.47.33.38:19190\naddnode=193.47.33.37:19190\naddnode=5.188.205.161:19190\n" >> "/root/.fonerocore/fonero.conf"
 sleep 3
 sudo sed -i -e "s/exit 0/sudo \-u root \/opt\/fonero-core\/fonerod \> \/dev\/null \&\nexit 0/g" /etc/rc.local
 ./fonerod -daemon
